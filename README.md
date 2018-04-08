e2dde5f09986289b1c3a8fdaf575a38ea0ad56af
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-04-08 23:19:17 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,14 @@
+# fe-bp
+
+This is a frontend boilerplate based on **unejected** create-react-app. It currently is capable of:
+
+- CRA (Everything create-react-app can do)
+- eslint
+- SASS
+- Jasmine
+
+## Notice
+
+Use it for whatever you want.
+
+Hendrik Ulbrich © 2018
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to <b>fe-bp</b></h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>App.js</code> or <code>App.scss</code>and save
+        to reload. <code>npm start</code> will run on two threads.
+      </p>
+    </div>
+  );
+}
