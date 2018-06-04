409c8051f6d8651475510d4daf01d039f53e02dd
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-06-05 00:36:20 +0200

    Add missing space

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <h1 className="App-title">Welcome to <b>fe-bp</b></h1>
       </header>
       <p className="App-intro">
-        To get started, edit <code>App.js</code> or <code>App.scss</code>and save
+        To get started, edit <code>App.js</code> or <code>App.scss</code> and save
         to reload. <code>npm start</code> will run on two threads.
       </p>
     </div>
