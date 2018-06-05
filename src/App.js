369419b2e6f7a1eff5dc3a3aa7bea7615bd09c7c
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-06-05 13:22:26 +0200

    Improve material-ui samples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { lightTheme, darkTheme, alternativeTheme } from './Themes';
-import logo from './logo.svg';
+import NavigationBar from './NavigationBar';
+import Header from './Header';
+import Body from './Body';
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,47 +14,22 @@ export default class App extends React.Component {
     };
   }
 
+  setTheme(themeString) {
+    let theme;
+    if (themeString === 'dark') {
+      theme = darkTheme;
+    } else {
+      theme = themeString === 'light' ? lightTheme : alternativeTheme;
+    }
+    this.setState({ theme });
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={this.state.theme}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to <b>fe-bp</b></h1>
-        </header>
-        <p className="App-intro">
-          #material-ui
-        </p>
-        <p className="App-intro">
-          To get started, edit <code>App.js</code> or <code>App.scss</code> and save
-          to reload. <code>npm start</code> will run on two threads.
-        </p>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            this.setState({ theme: lightTheme });
-          }}
-        >
-          Light <Icon >highlight</Icon>
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            this.setState({ theme: darkTheme });
-          }}
-        >
-          Dark <Icon >highlight</Icon>
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            this.setState({ theme: alternativeTheme });
-          }}
-        >
-          Alternative <Icon >highlight</Icon>
-        </Button>
+        <NavigationBar title="#material-ui" />
+        <Header />
+        <Body setTheme={this.setTheme.bind(this)} />
       </MuiThemeProvider>
     );
   }
