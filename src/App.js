062339ab7dff1928fff184c11e3d7107fca9e260
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-06-05 11:49:22 +0200

    Basic material theme implementation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { lightTheme, darkTheme, alternativeTheme } from './Themes';
 import logo from './logo.svg';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to <b>fe-bp</b></h1>
-      </header>
-      <p className="App-intro">
-        #material-ui
-      </p>
-      <p className="App-intro">
-        To get started, edit <code>App.js</code> or <code>App.scss</code> and save
-        to reload. <code>npm start</code> will run on two threads.
-      </p>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: darkTheme,
+    };
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider theme={this.state.theme}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to <b>fe-bp</b></h1>
+        </header>
+        <p className="App-intro">
+          #material-ui
+        </p>
+        <p className="App-intro">
+          To get started, edit <code>App.js</code> or <code>App.scss</code> and save
+          to reload. <code>npm start</code> will run on two threads.
+        </p>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            this.setState({ theme: lightTheme });
+          }}
+        >
+          Light <Icon >highlight</Icon>
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            this.setState({ theme: darkTheme });
+          }}
+        >
+          Dark <Icon >highlight</Icon>
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            this.setState({ theme: alternativeTheme });
+          }}
+        >
+          Alternative <Icon >highlight</Icon>
+        </Button>
+      </MuiThemeProvider>
+    );
+  }
 }
